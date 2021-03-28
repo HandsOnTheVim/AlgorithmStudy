6f0ea949b451e60c9f6a7b4228d29443d1cca937
Sunha03 <ahnus123@gmail.com>
2021-03-28 16:49:49 +0900

    Revert "Code Refactoring"
    This reverts commit 4f7000773548e9c6fcdd26abddfe70b3ad40d8ba.

--- 2021_03_14/ParkSunHa/programmers_70129_RepeatBinaryConversion.java
+++ 2021_03_14/ParkSunHa/programmers_70129_RepeatBinaryConversion.java
@@ -20,18 +20,19 @@ public class programmers_70129_RepeatBinaryConversion {
         int zeroCnt = 0;
         int loopCnt = 0;
         
-        while ("1".equals(s) == false) {
-        	for (int i = 0; i < s.length(); i++) {		// 0 지우기
-        		if (s.charAt(i) == '0') {
+        while(!s.equals("1")) {
+        	for(int i=0;i<s.length();i++) {		// 0 지우기
+        		if(s.charAt(i) == '0')
         			zeroCnt++;
-        		}
         	}
     		s = s.replaceAll("0", "");
+        	//System.out.println(zeroCnt + " ==== " + s);
         	
         	int sLength = s.length();	// 0 제거한 문자열의 길이
-        	if (sLength > 1) {
+        	if(sLength > 1) {
 	        	//s = binaryConv(sLength);	// 수정된 문자열 길이를 2진법으로 표시 (직접 함수 구현)
         		s = Integer.toBinaryString(sLength);	// 수정된 문자열 길이를 2진법으로 표시(내장함수 toBinaryString())
+	        	//System.out.println(loopCnt + " -> " + s);
         	}
         	loopCnt++;
         }
@@ -44,25 +45,24 @@ public class programmers_70129_RepeatBinaryConversion {
     }
     
     public static String binaryConv(int sLength) {		// 10진법 -> 2진법
-    	StringBuilder res = new StringBuilder("");
+    	String res = "";
     	Stack<Integer> binary = new Stack<>();
     	
-    	if (sLength == 1) {
+    	if(sLength == 1)
     		return "1";
-    	}
     	
-    	while (sLength >= 2) {
+    	while(sLength >= 2) {
     		binary.add(sLength%2);
     		sLength /= 2;
     	}
     	binary.add(sLength);
     	
     	int size = binary.size();
-    	for (int i = 0; i < size; i++) {
+    	for(int i=0;i<size;i++) {
     		int n = binary.pop();
-    		res.append(String.valueOf(n));
+    		res += String.valueOf(n);
     	}
     	
-    	return res.toString();
+    	return res;
     }
 }
